cabe6bc2df78ef436c2c7f09a5b588fe98006cc7
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 12:17:52 +0100

    ajout exo5

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
-<?php
-$maVariable = 'homme';
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 5 partie 3 php</title>
+  </head>
+  <body>
+    <?php
+    $maVariable = 'homme';
 
-if ($maVariable == 'homme') {
-  echo ('C\'est un développeur');
-}
-elseif ($maVariable == 'femme') {
-  echo ('C\'est une développeuse');
-}
-else {
-  echo ('IT\'S A TRAP');
-}
-?>
+    if ($maVariable == 'homme') {
+      echo ('C\'est un développeur');
+    }
+    elseif ($maVariable == 'femme') {
+      echo ('C\'est une développeuse');
+    }
+    else {
+      echo ('IT\'S A TRAP');
+    }
+    ?>
+  </body>
+</html>
