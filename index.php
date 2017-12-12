5b2eae601579a8a4ff13e9316c7817d0044e7ee0
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 11:07:01 +0100

    ajout exo5

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$maVariable = 'homme';
+
+if ($maVariable == 'homme') {
+  echo ('C\'est un développeur');
+}
+elseif ($maVariable == 'femme') {
+  echo ('C\'est une développeuse');
+}
+else {
+  echo ('IT\'S A TRAP');
+}
+?>
