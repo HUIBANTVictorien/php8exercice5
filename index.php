352d3dfca4f5c745058721f28678cc9cfa2d18d4
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-13 09:57:16 +0100

    ajout exo5

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>exercice 5 partie 3 php</title>
-  </head>
+<head>
+  <meta charset="utf-8">
+  <title>exercice 5 partie 7 php</title>
 </head>
 <body>
-  <?php
+  <form class="form" action="index.php" method="post">
+    <select class="civility" name="civility">
+      <option value="Monsieur" name="Monsieur">Monsieur</option>
+      <option value="Madame" name="Madame">Madame</option>
+    </select>
+    <input type="lastname" name="lastname" placeholder="Nom">
+    <input type="firstname" name="firstname" placeholder="Prénom">
+    <button type="submit" name="validate">Valider</button>
+  </form>
+  <p><?php
+  if (isset($_POST['civility']) && ($_POST['lastname']) && ($_POST['firstname'])) {
+    echo 'Bonjour ' . $_POST['civility'] . ' ' . $_POST['lastname'] . ' ' . $_POST['firstname'];
+  }
+  ?>
+</p>
 
-  $text = 'On y arrive presque';
-    for ($number = 1; $number < 15; $number++) {
-      echo $text.'<br/>';
-    }
-    if ($number == 15) {
-      echo ('On y est arrivé !');
-    }
-   ?>
+
 </body>
 </html>
